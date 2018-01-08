e7a568784a11ca99850faa045ffb778b82c23c01
Alex Bullock <alb5433@student.hks.harvard.edu>
2018-01-08 12:25:32 -0500

    add functions to calculator

---
+++ calculator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python2
+x,= raw_input("Value_1? " )
+y = raw_input("Value_2? " )
+x = int(x)
+y = int(y)
+def multiply (x,y):
+	return (x * y)
+def add (x,y):
+	return (x + y)
+def subtract (x,y):
+	return (x - y)
+def divide (x,y):
+	return (x / y)
+
